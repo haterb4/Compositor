09780bcef5c9523b3ae70fe2a92e05da18889584
haterb4 <haterb2803@gmail.com>
2023-01-01 17:03:01 +0100

    login page hanfling events

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,22 +1,73 @@
 import { Button } from '@material-tailwind/react'
-import React from 'react'
+import React, { useState } from 'react'
 import AuthLayout from '../layouts/AuthLayout'
 import styles from '../styles/Login.module.css'
 
-const login = () => {
+type ChildProps = {}
+
+const login= () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState(false)
+  const [emailError, setEmailError] = useState("false")
+  const [passwordError, setPasswordlError] = useState("false")
+
+  const Login = () => {
+    if (email.length === 0){
+        setError(true)
+        setEmailError("Email is required")
+    }
+    if (password.length === 0){
+        setError(true)
+        setPasswordlError("Password is required")
+    }
+    if (email && password){
+        let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+        if (email.match(mailformat)){
+            if (password.length < 8){
+                setError(true)
+                setPasswordlError("Password is too short")
+            }
+            else{
+                alert('ok')
+            }
+        }
+        else{
+            setError(true)
+            setEmailError("Email is not valid email format")
+        }
+    }
+  }
+  const resetError = () => {
+    setError(false)
+    setEmailError("")
+    setPasswordlError("")
+  }
   return (
     <AuthLayout title='login Page'>
         <div className={styles.loginForm}>
             <h1 className='font-bold text-xl'>Connexion</h1>
             <div className='w-full flex flex-col justify-between items-center py-4 mt-4 h-64'>
-                <div className='w-full'>
-                    <input type="text" name="" id="" placeholder='Email' className='w-full h-12 border outline-none px-3'/>
+                <div className={error?styles.loginFieldErr:styles.loginField}>
+                    <input type="text" name="" id="" placeholder='Email'
+                        className='w-full h-12 border outline-none px-3'
+                        onChange={ e => setEmail(e.target.value)}
+                        onClick={resetError}
+                    />
+                    {emailError && (<p className='my-1 text-xs'>{emailError}</p>)}
+                    {email && <span className='text-xs'>Email</span>}
                 </div>
-                <div className='w-full'>
-                    <input type="text" name="" id="" placeholder='Mot de passe' className='w-full h-12 border outline-none px-3'/>
+                <div className={error?styles.loginFieldErr:styles.loginField}>
+                    <input type="password" name="" id="" placeholder='Mot de passe'
+                        className='w-full h-12 border outline-none px-3'
+                        onChange={ e => setPassword(e.target.value)}
+                        onClick={resetError}
+                    />
+                    {passwordError && (<p className='my-1 text-xs'>{passwordError}</p>)}
+                    {password && <span className='text-xs'>Mot de passe</span>}
                 </div>
                 <div>
-                    <Button className='mt-8'>Se connecter</Button>
+                    <Button className='mt-8' onClick={Login}>Se connecter</Button>
                 </div>
             </div>
         </div>
