e88b413f193f453eab6b200f294115fea52cefa2
The-Professor-Alfred-Hetsron <parkeryepnjio@gmail.com>
2023-06-17 11:19:40 +0100

    Added Tailwind CSS

---
+++ xccm-compositor/src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './LandingPage.css'
+
+const LandingPage = () => {
+  return (
+    <div>
+        LandingPage
+        Hello Alfred
+    </div>
+  )
+}
+
+export default LandingPage
